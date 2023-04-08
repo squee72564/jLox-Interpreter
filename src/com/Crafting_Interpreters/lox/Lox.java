c17bfe2ae898232f6d57f3ec0adac568eaeb7f6f
squee72564 <rodriguez.al183@gmail.com>
2023-04-08 14:19:10 -0700

    Added rudimentary error handling functions

--- src/com/Crafting_Interpreters/lox/Lox.java
+++ src/com/Crafting_Interpreters/lox/Lox.java
@@ -9,9 +9,12 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(Stirng[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
+            System.exit(64);
         } else if (args.length == 1) {
             runFile(args[0]);
         } else {
@@ -22,6 +25,8 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError)
+            System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -33,7 +38,9 @@ public class Lox {
             String line = reader.readLine();
             if (line == null)
                 break;
+
             run(line);
+            hadError = false;
         }
     }
 
@@ -44,5 +51,14 @@ public class Lox {
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line" + line + "] Error " + where + ": " + message);
+        hadError = true;
     }
 }
