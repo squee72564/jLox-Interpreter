cb4b77cf1b4718023749d3b9dc2fc068c05fad06
squee72564 <rodriguez.al183@gmail.com>
2023-04-11 21:25:32 -0700

    Added code to generate visitors for expressions.
    We can use the polymorphic dispatch of the Expression class to select the appropriate method on the visitor class.

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -31,6 +31,8 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " { ");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
@@ -38,8 +40,23 @@ public class GenerateAst {
             defineType(writer, baseName, className, fields);
         }
 
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println(
+                    "        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
     }
 
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -55,6 +72,12 @@ public class GenerateAst {
         }
 
         writer.println("        }");
+
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("        return visitor.visit" + className + baseName + "(this);");
+        writer.println("    }");
 
         // Fields
         writer.println();
